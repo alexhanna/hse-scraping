a114669ed4a4854605833a4214ec2812faeb61c0
Alex Hanna <alex.hanna@gmail.com>
2013-08-17 02:13:03 +0000

    Fixing source URL. Adding some more exclusion rules

--- blogcrawler/blogcrawler/spiders/blogspider.py
+++ blogcrawler/blogcrawler/spiders/blogspider.py
@@ -8,33 +8,29 @@ from urlparse import urlparse
 class BlogSpider(CrawlSpider):    
     name = "blogspider"
     allowed_domains = [
-        "wordpress.org", 
+        "wordpress.com", 
         "blogspot.com", 
         "blogger.com",
         "livejournal.com",
         "typepad.com", 
         "tumblr.com"]
 
-    ## TK: Change this so we cna get arguments from the command line
     start_urls = ["http://badhessian.org"]
-
-    curr_url = urlparse(start_urls[0])[1]
 
     rules = (
         Rule(SgmlLinkExtractor(
             allow=('/', ),
             deny=('www\.blogger\.com', 'profile\.typepad\.com', 
-                'http:\/\/wordpress\.com', '[A-Za-z]+\.trac\.wordpress\.com',
-                '.+\.wordpress\.org', 'wordpress\.org', 'www\.tumblr\.com', ),
+                'http:\/\/wordpress\.com', '.+\.trac\.wordpress\.org',
+                '.+\.wordpress\.org', 'wordpress\.org', 'www\.tumblr\.com', 
+                'en\..+\.wordpress\.com', 'vip\.wordpress\.com'),
                 ), callback = "parse_item", follow = True), 
     )
 
     def parse_item(self, response):
         item = BlogcrawlerItem()
 
-        item['url1'] = self.curr_url
+        item['url1'] = urlparse(response.request.headers.get('Referer'))[1]
         item['url2'] = urlparse(response.url)[1]
-
-        self.curr_url = item['url2']
 
         yield item
